28259340507b92ae35abedd117af8c3c39f740dd
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-03-25 17:59:22 +0100

    design discussion

--- app/makeClient.tsx
+++ app/makeClient.tsx
@@ -1,5 +1,6 @@
-import { HttpLink } from "@apollo/client/index.js";
+import { HttpLink, createQueryPreloader } from "@apollo/client/index.js";
 import { ApolloClient, InMemoryCache } from "@apollo/client-react-streaming";
+import { LoaderFunction, LoaderFunctionArgs, defer } from "@remix-run/node";
 
 export function makeClient() {
   return new ApolloClient({
@@ -9,3 +10,14 @@ export function makeClient() {
     }),
   });
 }
+
+type In = LoaderFunctionArgs & {
+  preload: ReturnType<typeof createQueryPreloader>;
+};
+type LoaderCreator = (<T>(
+  fn: (args: In) => T
+) => (args: LoaderFunctionArgs) => T) & { defer: typeof defer };
+// pattern similar to https://github.com/kirill-konshin/next-redux-wrapper?tab=readme-ov-file#getstaticprops
+declare function makeLoaderCreator(make: typeof makeClient): LoaderCreator;
+
+export const apolloLoader = makeLoaderCreator(makeClient);
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,17 @@
-import type { MetaFunction } from "@remix-run/node";
-import { gql, useSuspenseQuery } from "@apollo/client/index.js";
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import {
+  createQueryPreloader,
+  gql,
+  useApolloClient,
+  useSuspenseQuery,
+} from "@apollo/client/index.js";
 import { Suspense } from "react";
+import {
+  ClientLoaderFunctionArgs,
+  defer,
+  useLoaderData,
+} from "@remix-run/react";
+import { apolloLoader, makeClient } from "~/makeClient";
 
 export const meta: MetaFunction = () => {
   return [
@@ -30,7 +41,61 @@ const ALL_FILMS = gql`
   }
 `;
 
+function middleware(bar: { query: any; variables: any; result: Promise<any> }) {
+  return {
+    bar: { query, variables },
+    bar__promise: result,
+  };
+}
+
+export const loader = apolloLoader(async ({ preload }) => {
+  // we could get around this if we could add dehydration logic to `defer` on a framework level
+  return apolloLoader.defer({
+    bar: preload(ALL_FILMS),
+    baz: await preload(ALL_FILMS).toPromise(),
+    boo: preload(ALL_FILMS).toPromise(), // runtime warning for this
+  });
+  return preload(ALL_FILMS);
+  return preload(ALL_FILMS).toPromise();
+});
+
+export const _loader = ({ context }: LoaderFunctionArgs) => {
+  const loader = createQueryPreloader(makeClient(context));
+
+  return defer(
+    middleware({
+      bar: loader(ALL_FILMS),
+      // ==>
+      // bar: {
+      //   query: ALL_FILMS,
+      //   variables: {},
+      //   result: client.query(ALL_FILMS),
+      //   async toPromise() {
+      //    return { query, variables, result: await this.result }
+      //   }
+      // },
+      foo: Promise.resolve("asd"),
+    })
+  );
+};
+
+// export const clientLoader = ({serverLoader, context}: ClientLoaderFunctionArgs) => {
+//   return {}
+// }
+
+function useApolloLoaderData() {
+  // we could get around this if we could add useApolloClient logic to `defer` on a framework level
+  const client = useApolloClient();
+  client.simulateQuery({ query, variables });
+  dataPromise.then((result) =>
+    client.simulateResult({ query, variables, result })
+  );
+  const realPreloader = createQueryPreloader(client);
+  return realPreloader(query, { variables });
+}
+
 export default function Index() {
+  const { foo, bar } = useApolloResult(useLoaderData<typeof loader>());
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
